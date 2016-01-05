4f721e7e5031747ce58ba22371d2be9e996de383
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2016-01-05 14:25:58 -0500

    Update Exploratory.R

--- Exploratory.R
+++ Exploratory.R
@@ -14,7 +14,7 @@ rm(news);rm(blogs);rm(twitter)
 profanity <- profanity$V1
 
 text <- gsub(pattern=';|\\.|!|\\?', x=text, replacement='ootoo')
-text <- gsub(pattern="[[:punct:]]",x=text,replacement="")
+text <- str_replace_all(text, "[[:punct:]]", "")
 text <- gsub(pattern="[^[:alpha:]]", x=text, replacement = ' ')
 text <- tolower(text)
 text <- gsub(pattern="\\W*\\b\\w{1,2}\\b", x=text, replacement=' ')
@@ -44,6 +44,7 @@ n1 <- Make_Ngrams(text,size=1)
 n2 <- Make_Ngrams(text,size=2)
 n3 <- Make_Ngrams(text,size=3)
 n4 <- Make_Ngrams(text,size=4)
+n5 <- Make_Ngrams(text,size=5)
 
 n1_df <- table(n1)
 n1_df <- as.data.frame(n1_df) %>%
@@ -61,32 +62,43 @@ n3_df <- as.data.frame(n3_df) %>%
 n4_df <- table(n4)
 n4_df <- as.data.frame(n4_df) %>%
   arrange(desc(Freq))
-  
-  n1_df$n1 <- as.character(n1_df$n1)
+
+n5_df <- table(n5)
+n5_df <- as.data.frame(n5_df) %>%
+  arrange(desc(Freq))
+n1_df$n1 <- as.character(n1_df$n1)
 n2_df$n2 <- as.character(n2_df$n2)
 n3_df$n3 <- as.character(n3_df$n3)
 n4_df$n4 <- as.character(n4_df$n4)
+n5_df$n5 <- as.character(n5_df$n5)
 
+five_gram <- data.frame((str_split_fixed(n5_df$n5, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
 four_gram <-data.frame((str_split_fixed(n4_df$n4, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
 three_gram <-data.frame((str_split_fixed(n3_df$n3, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
 two_gram <-data.frame((str_split_fixed(n2_df$n2, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
 
-sentence <- "When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd"
+
+sentence <- "Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his"
 sentence <- str_replace_all(sentence, "[[:punct:]]", "")
 sentence <- tolower(sentence)
 sentence <- gsub(pattern="\\s+", x=sentence, replacement=' ')
 
 sentence<-data.frame(setNames(strsplit(x = sentence, split = " "),nm="x"),stringsAsFactors=FALSE)
-sentence <- if(length(sentence$x) >=3) {
-  tail(sentence,3)} else{
+sentence <- if(length(sentence$x) >=4) {
+  tail(sentence,4)} else{
     tail(sentence)}
 sentence <- unlist(sentence)
 sentence <- data.frame(sentence,stringsAsFactors=FALSE)
 
-word <- c(paste(sentence$sentence,collapse=" "))
-word2 <- c(paste(sentence[2,1],sentence[3,1],collapse=" "))
 
-if(length(sentence$sentence) == 3){
+word <- c(paste(sentence$sentence,collapse=" "))
+word2 <- c(paste(sentence[2,1],sentence[3,1],sentence[4,1],collapse=" "))
+word3 <- c(paste(sentence[2,1],sentence[3,1],collapse=" "))
+
+if(length(sentence$sentence) == 4){
+  match <- five_gram[five_gram$X1 == word,]
+  match <- match[1,2]
+} else if(length(sentence$sentence) == 3){
   match <- four_gram[four_gram$X1 == word,]
   match <- match[1,2]
 } else if(length(sentence$sentence) == 2){
@@ -99,12 +111,20 @@ if(length(sentence$sentence) == 3){
   match <- NA
 }
 
-if(is.na(match) == TRUE & length(sentence$sentence)==3){
-  match <- three_gram[three_gram$X1 == word2,]
+if(is.na(match) == TRUE && length(sentence$sentence)==4){
+  match <- four_gram[four_gram$X1 == word2,]
   match <- match[1,2]
-} 
+}
+if(is.na(match) == TRUE && length(sentence$sentence)>=3){
+  match <- three_gram[three_gram$X1 == word3,]
+  match <- match[1,2]
+}
 
 if(is.na(match) == TRUE){
   match <- "the"
 }
+match
+
+  
+  
 
