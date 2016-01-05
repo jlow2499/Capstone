c98cbcb0c834a6bbf314362d6003fe11e4fca59b
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2016-01-05 13:27:42 -0500

    Update Exploratory.R

--- Exploratory.R
+++ Exploratory.R
@@ -1,45 +1,28 @@
 library(ngram)
 library(dplyr)
-news <- readLines("C:/Users/Jeremiah Lowhorn/Desktop/final/en_US/en_US.news.txt", 10000,encoding="UTF-8")
-blogs <- readLines("C:/Users/Jeremiah Lowhorn/Desktop/final/en_US/en_US.blogs.txt", 10000,encoding="UTF-8")
-twitter <- readLines("C:/Users/Jeremiah Lowhorn/Desktop/final/en_US/en_US.twitter.txt", 10000,encoding="UTF-8")
-profanity <- read.csv("C:/Users/Jeremiah Lowhorn/Desktop/final/en_US/profanity.txt", header=FALSE, stringsAsFactors=FALSE)
+library(ggplot2)
+library(stringr)
+news <- readLines("C:/Users/193344/Desktop/final/en_US/en_US.news.txt", 20000,encoding="UTF-8")
+blogs <- readLines("C:/Users/193344/Desktop/final/en_US/en_US.blogs.txt", 20000,encoding="UTF-8")
+twitter <- readLines("C:/Users/193344/Desktop/final/en_US/en_US.twitter.txt", 20000,encoding="UTF-8")
+profanity <- read.csv("C:/Users/193344/Desktop/final/en_US/profanity.txt", header=FALSE, stringsAsFactors=FALSE)
 
-text <- c(news,blogs,twitter)
+text <- c(news,twitter,blogs)
+
 rm(news);rm(blogs);rm(twitter)
 
 profanity <- profanity$V1
 
-Clean <- function(text_blob) {
-  
-  text_blob <- gsub(';|\\.|!|\\?', 
-                    text_blob, 
-                    'ootoo')
-   
-  text_blob <- gsub(paste0('\\<',profanity,collapse="|"),
-                    text_blob,
-                     "expletive ",
-                    ignore.case=TRUE)
-  
-  text_blob <- gsub("[^[:alpha:]]",
-                    text_blob,
-                     ' ')
-  
-  text_blob <- tolower(text_blob)
-
-  text_blob <- gsub("\\W*\\b\\w{1,2}\\b",
-                    text_blob,
-                    ' ')
-  
-  text_blob <- gsub("\\s+",
-                    text_blob, 
-                    ' ')
-  
-  sentence_vector <- unlist(strsplit(text_blob,
-                                     split='ootoo',
-                                     fixed = TRUE))
-  return (sentence_vector)
-}
+text <- gsub(pattern=';|\\.|!|\\?', x=text, replacement='ootoo')
+text <- str_replace_all(text, "[[:punct:]]", "")
+text <- gsub(pattern="[^[:alpha:]]", x=text, replacement = ' ')
+text <- tolower(text)
+text <- gsub(pattern="\\W*\\b\\w{1,2}\\b", x=text, replacement=' ')
+text <- gsub(pattern="\\s+", x=text, replacement=' ')
+text <- unlist(strsplit(x=text, split='ootoo',fixed = TRUE))
+text <- gsub(pattern=paste0('\\<',profanity,collapse="|"),
+             x=text,
+             replacement="")
 
 Trim <- function( x ) {
   gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
@@ -57,13 +40,10 @@ Make_Ngrams <- function(sentence_splits, size=2) {
   return (ngrams)
 }
 
-a <- Clean(text)
-
-n1 <- Make_Ngrams(a,size=1)
-n2 <- Make_Ngrams(a,size=2)
-n3 <- Make_Ngrams(a,size=3)
-n4 <- Make_Ngrams(a,size=4)
-#n5 <- Make_Ngrams(a,size=5)
+n1 <- Make_Ngrams(text,size=1)
+n2 <- Make_Ngrams(text,size=2)
+n3 <- Make_Ngrams(text,size=3)
+n4 <- Make_Ngrams(text,size=4)
 
 n1_df <- table(n1)
 n1_df <- as.data.frame(n1_df) %>%
@@ -81,35 +61,55 @@ n3_df <- as.data.frame(n3_df) %>%
 n4_df <- table(n4)
 n4_df <- as.data.frame(n4_df) %>%
   arrange(desc(Freq))
+  
+  n1_df$n1 <- as.character(n1_df$n1)
+n2_df$n2 <- as.character(n2_df$n2)
+n3_df$n3 <- as.character(n3_df$n3)
+n4_df$n4 <- as.character(n4_df$n4)
 
-n_all <- c(n2,n3,n4)
 
-word <- 'montecristo '
+four_gram <-data.frame((str_split_fixed(n4_df$n4, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
+three_gram <-data.frame((str_split_fixed(n3_df$n3, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
+two_gram <-data.frame((str_split_fixed(n2_df$n2, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
 
-matches <- c()
-for (sentence in n_all) {
-  if (grepl(paste0('\\<',word), sentence)) {
-    print(sentence)
-    matches <- c(matches, sentence)
-  }
+
+sentence <- "When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd"
+sentence <- str_replace_all(sentence, "[[:punct:]]", "")
+sentence <- tolower(sentence)
+sentence <- gsub(pattern="\\s+", x=sentence, replacement=' ')
+
+sentence<-data.frame(setNames(strsplit(x = sentence, split = " "),nm="x"),stringsAsFactors=FALSE)
+sentence <- if(length(sentence$x) >=3) {
+  tail(sentence,3)} else{
+    tail(sentence)}
+sentence <- unlist(sentence)
+sentence <- data.frame(sentence,stringsAsFactors=FALSE)
+
+
+word <- c(paste(sentence$sentence,collapse=" "))
+word2 <- c(paste(sentence[2,1],sentence[3,1],collapse=" "))
+
+if(length(sentence$sentence) == 3){
+  match <- four_gram[four_gram$X1 == word,]
+  match <- match[1,2]
+} else if(length(sentence$sentence) == 2){
+  match <- three_gram[three_gram$X1 == word,]
+  match <- match[1,2]
+} else if(length(sentence$sentence)==1){
+  match <- two_gram[two_gram$X1 == word,]
+  match <- match[1,2]
+} else{
+  match <- NA
 }
 
-if(length(matches)==0){
-  final_match <- n1_df[1,1]
-}else{
+if(is.na(match) == TRUE & length(sentence$sentence)==3){
+  match <- three_gram[three_gram$X1 == word2,]
+  match <- match[1,2]
+} 
 
-split_match <- c()
-for (a_match in matches) {
-  split_match <- c(split_match,nchar(strsplit(x = a_match, split = word)[[1]][[1]]))
+
+if(is.na(match) == TRUE){
+  match <- "the"
 }
 
-final_matched_sentence <- sample(matches[split_match == max(split_match)],size = 1)
 
-print(final_matched_sentence)
-
-final_match <- strsplit(x = final_matched_sentence, split = word)[[1]]
-final_match <-  strsplit(x = final_match[[2]], split = " ")[[1]]
-final_match <- final_match[[1]]
-}
-
-print(final_match)
