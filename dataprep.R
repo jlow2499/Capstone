839eb73cdd43805628dfd63a9a4fbe6e2a8517af
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2016-01-07 13:42:34 -0500

    Update dataprep.R

--- dataprep.R
+++ dataprep.R
@@ -5,7 +5,8 @@ library(stringr)
 news <- readLines("C:/Users/193344/Desktop/final/en_US/en_US.news.txt", 10000,encoding="UTF-8")
 blogs <- readLines("C:/Users/193344/Desktop/final/en_US/en_US.blogs.txt", 10000,encoding="UTF-8")
 twitter <- readLines("C:/Users/193344/Desktop/final/en_US/en_US.twitter.txt", 10000,encoding="UTF-8")
-
+stopwords <- read.table("C:/Users/193344/Desktop/final/en_US/stopwords.csv", quote="\"", stringsAsFactors=FALSE)
+stopwords <- str_replace_all(stopwords$V1, "[[:punct:]]", "")
 profanity <- read.csv("C:/Users/193344/Desktop/final/en_US/profanity.txt", header=FALSE, stringsAsFactors=FALSE)
 
 text <- c(news,twitter,blogs)
