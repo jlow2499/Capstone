49cc6676af3236daa218ad6eb36d3718b9c6d1e8
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2016-01-07 13:37:52 -0500

    Delete ui.R

--- ui.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-shinyUI(fluidPage(
-  textInput("text", label = h2("Next Word Predictor"), value = "Hello how are"),
-  submitButton(text = "Predict next word..."),
-  hr(),
-  fluidRow((verbatimTextOutput("value")))
-  
-))
