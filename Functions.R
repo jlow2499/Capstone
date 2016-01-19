06810b222d196d60d364a58b03ca8be80c91c68f
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2016-01-19 12:04:46 -0500

    Update Functions.R

--- Functions.R
+++ Functions.R
@@ -1,89 +1,45 @@
-find_next_word <- function(sentence) { 
-  sentence <- str_replace_all(sentence, "[[:punct:]]", "")
-  sentence <- tolower(sentence)
-  sentence <- sub("\\s+$", "", sentence)
-  sentence <- gsub(pattern="\\s+", x=sentence, replacement=' ')
-  lastword <- word(sentence,-1)
+build_tables <- function(text,size) {
   
-  sentence<-data.frame(setNames(strsplit(x = sentence, split = " "),nm="x"),stringsAsFactors=FALSE)
-  sentence <- if(length(sentence$x) >=4) {
-    tail(sentence,4)} else{
-      tail(sentence)}
-  sentence <- unlist(sentence)
-  sentence <- data.frame(sentence,stringsAsFactors=FALSE)
+  n2 <- Make_Ngrams(text,size=size)
   
-  word <- c(paste(sentence$sentence,collapse=" "))
-  if(length(sentence$sentence)==4){
-    word2 <- c(paste(sentence[2,1],sentence[3,1],sentence[4,1],collapse=" "))
-    word3 <- c(paste(sentence[3,1],sentence[4,1],collapse=" "))
-    word4 <- c(paste(sentence[4,1],collapse=" "))
-  } else if(length(sentence$sentence)==3){
-    word2 <- c(paste(sentence[1,1],sentence[2,1],sentence[3,1],collapse=" "))
-    word3 <- c(paste(sentence[2,1],sentence[3,1],collapse=" "))
-    word4 <- c(paste(sentence[3,1],collapse=" "))  
-  } else if(length(sentence$sentence)==2){
-    word2 <- c(paste(sentence[1,1],sentence[2,1],collapse=" "))
-    word3 <- c(paste(sentence[2,1],collapse=" "))
-    word4 <- NA
-  } else{
-    word2 <- c(paste(sentence[1,1],collapse=" "))
-    word3 <- NA
-    word4 <- NA
-  }
+  n2_df <- table(n2)
+  n2_df<- as.data.frame(n2_df) %>%
+    arrange(desc(Freq))
   
-  if((lastword %in% stopwords)==TRUE){
-    five <- five_gram()[!five_gram()$X2 %in% stopwords,]
-    four <- four_gram()[!four_gram()$X2 %in% stopwords,]
-    three <- three_gram()[!three_gram()$X2 %in% stopwords,]
-    two <- two_gram()[!two_gram()$X2 %in% stopwords,]
-  } else{
-    five <- five_gram()
-    four <- four_gram()
-    three <- three_gram()
-    two <- two_gram()
-  }
+  n2_df$n2 <- as.character(n2_df$n2)
+  Freq <- n2_df$Freq
   
-  if(length(sentence$sentence) == 4){
-    match <- five[five$X1 == word,]
-    match <- match[1,2]
-  } else if(length(sentence$sentence) == 3){
-    match <- four[four$X1 == word,]
-    match <- match[1,2]
-  } else if(length(sentence$sentence) == 2){
-    match <- three[three$X1 == word,]
-    match <- match[1,2]
-  } else if(length(sentence$sentence)==1){
-    match <- two[two$X1 == word,]
-    match <- match[1,2]
-  } else{
-    match <- NA
+  table <-data.frame((str_split_fixed(n2_df$n2, ' (?=[^ ]+$)',2)),stringsAsFactors=FALSE)
+  table <- cbind(table,Freq)
+  table
+}
+
+Trim <- function( x ) {
+  gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
+}
+
+clean <- function(text) {
+  text <- gsub(pattern=';|\\.|!|\\?', x=text, replacement='ootoo')
+  text <- str_replace_all(text, "[[:punct:]]", "")
+  text <- gsub(pattern="[^[:alpha:]]", x=text, replacement = ' ')
+  text <- tolower(text)
+  #text <- gsub(pattern="\\W*\\b\\w{1,2}\\b", x=text, replacement=' ')
+  text <- gsub(pattern="\\s+", x=text, replacement=' ')
+  text <- unlist(strsplit(x=text, split='ootoo',fixed = TRUE))
+  text <- gsub(pattern=paste0('\\<',profanity,collapse="|"),
+               x=text,
+               replacement="")
+  text
+}
+
+Make_Ngrams <- function(sentence_splits, size=2) {
+  ngrams <- c()
+  for (sentence in sentence_splits) {
+    sentence <- Trim(sentence)
+    if ((nchar(sentence) > 0) && (sapply(gregexpr("\\W+", sentence), length) >= size)) {
+      ngs <- ngram(sentence , n=size)
+      ngrams <- c(ngrams, get.ngrams(ngs))
+    }
   }
-  
-  if(is.na(match) == TRUE && length(sentence$sentence)==4){
-    match <- four[four$X1 == word2,]
-    match <- match[1,2]
-  }
-  if(is.na(match) == TRUE && length(sentence$sentence)>=3){
-    match <- three[three$X1 == word3,]
-    match <- match[1,2]
-  }
-  
-  if(is.na(match) == TRUE && length(sentence$sentence)>=2){
-    match <- two[two$X1 == word4,]
-    match <- match[1,2]
-  }
-  
-  if(is.na(match) == TRUE && length(sentence$sentence)>=2){
-    match <- two[two$X1 == word3,]
-    match <- match[1,2]
-  }
-  
-  if(is.na(match) == TRUE && length(sentence$sentence)>=1){
-    match <- "and"
-  }
-  
-  if(is.na(match) == TRUE){
-    match <- "the"
-  }
-  match
+  return (ngrams)
 }
